f8f20ecda2ac6babb7ad2588cdd57a04cff7aa08
Keks <keks@htmlacademy.ru>
2023-11-06 17:43:03 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/main.js
+++ 8/js/main.js
@@ -1,4 +1,7 @@
 import './data.js';
-import './rendering-pictures.js';
+import './thumbnail.js';
 import './full-photo-modal.js';
-import { pictureList } from './rendering-pictures.js';
+import { getPhotoPost } from './data.js';
+import { renderGallery } from './gallery.js';
+
+renderGallery(getPhotoPost());
