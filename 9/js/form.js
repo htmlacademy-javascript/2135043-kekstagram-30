89841eefc1d16e8f3582cd996d0b0442bed25be1
Keks <keks@htmlacademy.ru>
2023-11-08 19:28:30 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/form.js
+++ 9/js/form.js
@@ -1,10 +1,26 @@
 import { bodyElement } from './full-photo-modal.js';
-import { isEscapeKey } from './util.js';
+
+const MAX_HASHTAG_COUNT = 5;
+const VALID_SIMBOLS = /^#[a-zа-яё0-9]{1,19}$/i;
+const ErrorText = {
+  INVALID_COUNT: `Максимум ${MAX_HASHTAG_COUNT} хэштегов`,
+  NOT_UNIQUE: 'Хэштеги должны быть уникальными',
+  INVALID_PATTERN: 'Неверный хэштег',
+};
 
 const pictureForm = document.querySelector('.img-upload__form');
 const pictureUploadContainer = pictureForm.querySelector('.img-upload__overlay');
 const pictureOpeninput = pictureForm.querySelector('.img-upload__input');
 const pictureCloseButton = pictureForm.querySelector('.img-upload__cancel');
+const form = document.getElementById('upload-select-image');
+const hashtagField = pictureForm.querySelector('.text__hashtags');
+const commentField = pictureForm.querySelector('.text__description');
+
+const pristine = new Pristine(pictureForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper--error',
+});
 
 const showForm = () => {
   pictureUploadContainer.classList.remove('hidden');
@@ -12,34 +28,74 @@ const showForm = () => {
   document.addEventListener('keydown', onDocumentKeydown);
 };
 
-const onOpenPictureInputClick = () => {
-  showForm();
-};
-
 const closeForm = () => {
+  form.reset();
+  pristine.reset();
   pictureUploadContainer.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
+};
 
-  const form = document.getElementById('upload-select-image');
-  const inputs = form.document.getElementsByTagName('input');
-  for (let i = 0; i < inputs.length; i++) {
-    inputs[i].value = '';
-  }
+const isTextFieldFocused = () => document.activeElement === hashtagField || document.activeElement === commentField;
+
+const normalizeTags = (tagString) => tagString
+  .trim()
+  .split(' ')
+  .filter((tag) => Boolean(tag.length));
+
+const hasValidTags = (value) => normalizeTags(value).every((tag) => VALID_SIMBOLS.test(tag));
+
+const hasValidCount = (value) => normalizeTags(value).length <= MAX_HASHTAG_COUNT;
+
+const hasUniqueTags = (value) => {
+  const lowerCaseTags = normalizeTags(value).map((tag) => tag.toLowerCase());
+  return lowerCaseTags.length === new Set(lowerCaseTags).size;
 };
 
 function onDocumentKeydown(evt) {
-  if (isEscapeKey) {
+  if (evt.key === 'Escape' && !isTextFieldFocused()) {
     evt.preventDefault();
     closeForm();
   }
 }
 
+const onPictureInputChange = () => {
+  showForm();
+};
+
 const onClosePictureButtonClick = () => {
   closeForm();
 };
 
-pictureOpeninput.addEventListener('click', onOpenPictureInputClick);
-pictureCloseButton.addEventListener('click', onClosePictureButtonClick);
+const onFormSubmit = (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+};
 
-export { pictureForm };
+pristine.addValidator(
+  hashtagField,
+  hasValidCount,
+  ErrorText.INVALID_COUNT,
+  3,
+  true
+);
+
+pristine.addValidator(
+  hashtagField,
+  hasUniqueTags,
+  ErrorText.NOT_UNIQUE,
+  2,
+  true
+);
+
+pristine.addValidator(
+  hashtagField,
+  hasValidTags,
+  ErrorText.INVALID_PATTERN,
+  1,
+  true
+);
+
+pictureOpeninput.addEventListener('change', onPictureInputChange);
+pictureCloseButton.addEventListener('click', onClosePictureButtonClick);
+form.addEventListener('submit', onFormSubmit);
